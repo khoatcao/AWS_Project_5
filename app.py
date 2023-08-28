43f8624400f1267e210850afb1b7e0d74390d4bb
qdunggs95 <54254434+qdunggs95@users.noreply.github.com>
2023-08-28 23:11:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
-
+error_variable
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
